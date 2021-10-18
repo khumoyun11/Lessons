475c10754eb588a58e5dff9536b394537d22f6f8
Xumoyun <khumoyunabdullaev20@gmail.com>
2021-10-18 20:58:19 +0500

    lesson10

--- lesson10/Node.cs
+++ lesson10/Node.cs
@@ -7,6 +7,5 @@ namespace lesson10
         {
             Data = data;
         }
-        
     }
 }
--- lesson10/Program.cs
+++ lesson10/Program.cs
@@ -1,14 +1,51 @@
 ﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using lesson10.Dto.PrayerTime;
+using lesson10.Dto.User;
+using lesson10.Services;
 
 namespace lesson10
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string usersApi = "https://randomuser.me/api/";
+        private static string prayerTimeApi = "http://api.aladhan.com/v1/timingsByCity?city=Tashkent&country=Uzbekistan&method=8";
+        static async Task Main(string[] args)
         {
-            var node = new Node<string>("5");
-            var node2 = new Node<int>(5);
-            node2.Data = 4;
+            var httpService = new HttpClientService();
+            var result = await httpService.GetObjectAsync<PrayerTime>(prayerTimeApi);
+
+            if(result.IsSuccess)
+            {
+                var settings = new JsonSerializerOptions()
+                {
+                    WriteIndented = true
+                };
+
+                var json = JsonSerializer.Serialize(result.Data, settings);
+                Console.WriteLine($"{json}");
+            }
+            else
+            {
+                Console.WriteLine($"{result.ErrorMessage}");
+            }
+            
+        }
+        static async Task Main_user(string[] args)
+        {
+            var httpService = new HttpClientService();
+            var result = await httpService.GetObjectAsync<User>(usersApi);
+
+            if(result.IsSuccess)
+            {
+                Console.WriteLine($"{result.Data.Results[0].Name.First}");
+            }
+            else
+            {
+                Console.WriteLine($"{result.ErrorMessage}");
+            }
+            
         }
     }
 }
